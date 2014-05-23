75bd97b5565db4b6bb6edf81d9a221155129b035
Geoff Hing <geoffhing@gmail.com>
2014-05-23 18:21:52 -0500

    Add view for adding text version of cell
    Also add message flashing to template and breadcrumbs

--- tablesplitter/web.py
+++ tablesplitter/web.py
@@ -1,14 +1,17 @@
-from flask import (Flask, abort, render_template, send_from_directory)
+from flask import (Flask, abort, render_template, send_from_directory, request,
+                   flash, redirect, url_for)
 
 from tablesplitter.api import CellResource, TextResource
 from tablesplitter.conf import settings
-from tablesplitter.models import ImageFile, Project, SourceFile
+from tablesplitter.models import ImageFile, Project, SourceFile, SplitFile, Text
 
 app = Flask(__name__)
 app.config.from_object(__name__)
 app.config.from_object(settings)
 
 app.config.from_envvar('TABLESPLITTER_SETTINGS_MODULE', silent=True)
+
+app.secret_key = app.config['SECRET_KEY']
 
 
 @app.route('/')
@@ -28,12 +31,52 @@ def project(slug):
 @app.route('/source-files/<id>')
 def source_file(id):
     source_file = SourceFile.get(SourceFile.id == id)
-    return render_template('source_file.html', source_file=source_file)
+    project = source_file.project
+    breadcrumbs = [
+        (project.name, url_for('project', slug=project.slug)),
+        (source_file.filename, None),
+    ]
+    return render_template('source_file.html', source_file=source_file,
+        breadcrumbs=breadcrumbs)
 
 @app.route('/image-files/<id>')
 def image_file(id):
     image_file = ImageFile.get(ImageFile.id == id)
-    return render_template('image_file.html', image_file=image_file)
+    project = image_file.source.project
+    breadcrumbs = [
+        (project.name, url_for('project', slug=project.slug)),
+        (image_file.source.filename,
+         url_for('source_file', id=image_file.source.id)),
+        (image_file.filename, None),
+    ]
+    return render_template('image_file.html', image_file=image_file,
+        breadcrumbs=breadcrumbs)
+
+@app.route('/cells/<id>')
+def cell(id):
+    cell = SplitFile.get(SplitFile.id == id)
+    source_file = cell.source.source
+    project = source_file.project
+    label = "Row {}, Column {}".format(cell.row, cell.column)
+    breadcrumbs = [
+        (project.name, url_for('project', slug=project.slug)),
+        (source_file.filename,
+         url_for('source_file', id=source_file.id)),
+        (cell.source.filename, url_for('image_file', id=cell.source.id)),
+        (label, None),
+    ]
+    return render_template('cell.html', cell=cell, breadcrumbs=breadcrumbs)
+
+@app.route('/cells/<id>/add-text', methods=['POST'])
+def add_cell_text(id):
+    cell = SplitFile.get(SplitFile.id == id)
+    text = request.form['text']
+    Text.create(source=cell, method='manual',
+            text=text, user_id=request.form['user_id'])
+    flash("Text version '{}' added.".format(text), 'success')
+
+    return redirect(url_for('cell', id=id))
+ 
 
 @app.route('/img/<image_type>/<path:filename>')
 def image(image_type, filename):
