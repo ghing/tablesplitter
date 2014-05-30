3ac44ebcd31a791b2c16045358305dfa65e51e9f
Geoff Hing <geoffhing@gmail.com>
2014-05-30 09:16:31 -0500

    Create setup script
    Create setup.py file that uses setuptools to make this a
    distributable package.
    
    Define the dependencies in setup.py and use a pattern for
    requirements.txt from
    https://caremad.io/blog/setup-vs-requirement/

--- docs/installation.rst
+++ docs/installation.rst
@@ -4,3 +4,8 @@ Installation
 
 Tablesplitter requires Python 2.7+ or Python 3.3+.
 
+The best way to install tablesplitter is with pip:
+
+```
+pip install git+https://github.com/ghing/tablesplitter.git
+```
---
+++ setup.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+
+import os.path
+from setuptools import setup, find_packages
+
+with open(os.path.join(os.path.dirname(__file__), 'README.rst')) as f:
+    readme = f.read()
+
+setup(name='Tablesplitter',
+      version='0.0.1',
+      description='Extract data from tables in image PDFs.',
+      long_description=readme,
+      author='Geoff Hing',
+      author_email='geoffhing@gmail.com',
+      url='https://github.com/ghing/tablesplitter',
+      packages=find_packages(),
+      scripts=['bin/tsplit.py'],
+      install_requires=[
+          'pillow>=2.4',
+          'blinker>=1.3',
+          'peewee>=2.2.4',
+          'six>=1.6',
+          'restless>=2',
+          'flask>=0.10',
+      ],
+      classifiers=[
+          'Development Status :: 1 - Planning',
+          'Intended Audience :: Developers',
+          'License :: OSI Approved :: MIT License',
+          'Operating System :: OS Independent',
+          'Programming Language :: Python',
+          'Framework :: Flask',
+          ],
+     )
